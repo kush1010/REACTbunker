e4832892af738d01da8afa461871de596149c95f
kush1010 <56656941+kush1010@users.noreply.github.com>
2020-10-29 16:12:13 +0530

    Update scrlistcomp.js

--- scrlistcomp.js
+++ scrlistcomp.js
@@ -1,1 +1,43 @@
+import React from 'react';
+import {Text, StyleSheet, View, FlatList} from 'react-native';
 
+const ListScreen = () => {
+
+    const friends = [
+        {name: 'friend #1'}, 
+        {name: 'friend #2'},
+        {name: 'friend #3'},
+        {name: 'friend #4'},
+        {name: 'friend #5'},
+        {name: 'friend #6'},
+        {name: 'friend #7'},
+        {name: 'friend #8'},
+        {name: 'friend #9'},
+        {name: 'friend #10'}
+        
+    ];
+    
+    // here we are talking about making a list which is a very good thing i suppose....a
+    return <View>
+        <FlatList 
+            data= {friends} 
+            //the element that we are using in the next line is an object that 
+            // that contains item:{name:friend},index:0
+            renderItem={({item}) => {        
+
+                return<Text style={styles.listStyle}>{item.name}</Text>
+
+            }}   
+         />
+    </View>
+}
+    const styles = StyleSheet.create({
+        listStyle:{
+            fontSize:30,
+            color:"blue",
+            backgroundColor: "grey"
+        }
+    })
+
+
+export default ListScreen;
