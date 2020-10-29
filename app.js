e414510833d191b4a957211ee1d85d26f9107aac
kush1010 <56656941+kush1010@users.noreply.github.com>
2020-10-29 16:06:31 +0530

    app1.11

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from "./src/screens/HomeScreen";
+import componentscreens from "./src/screens/componentscreens";
+import ListScreen from './src/screens/listscreen'
+const navigator = createStackNavigator(
+  /* and we cant go back with what is love and what is lost so fellas comedown on
+  our tears and we can't go back with what is love and what is lost so fellas 
+  go back on our tears */
+
+  {
+    Home: HomeScreen,
+    Components : componentscreens,
+    list: ListScreen
+  },
+  {
+    initialRouteName: 'list',
+    defaultNavigationOptions: {
+      title: "App"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
