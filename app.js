dafc7a43376b0b59c96c52a84cc9d7ca4b75b046
aperipateticien <ankitsharma.satna@gmail.com>
2022-10-31 18:19:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import componentscreens from "./src/screens/componentscreens";
 import ListScreen from './src/screens/listscreen'
+import ListView from './src/views/';
 const navigator = createStackNavigator(
   /* and we cant go back with what is love and what is lost so fellas comedown on
   our tears and we can't go back with what is love and what is lost so fellas 
